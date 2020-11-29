6509e00ea4f82ff679b5d4545548ac8ffaa5a711
Alex King <alex.king@and.digital>
2020-11-29 14:44:01 +0000

    Initialise Maven project

---
+++ challenge-10-json-treasure-hunt/src/main/java/jsontreasurehunt/App.java
@@ -0,0 +1,13 @@
+package jsontreasurehunt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
